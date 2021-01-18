7e45e7e1d814e4a3a4095caf19190d4ad27b42b4
gouf <innocent.zero@gmail.com>
2021-01-18 21:25:55 +0900

    年・月のクエリパラメータを取得可能にした

---
+++ pages/admin/attendances.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <span>{{ this.$route.params }}</span>
+    <h2>{{ year }}年</h2>
+    <h2>{{ month }}月</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$nextTick(() => {
+      // const params = this.$route.params
+      // console.log('Year:', params.year)
+      // console.log('Month:', params.month)
+      // TODO: Axios でのリクエストコードの追加
+    })
+  },
+  // methods: {
+  //   year() {
+  //     return this.$route.params.year
+  //   },
+  // },
+  computed: {
+    year() {
+      return this.$route.params.year
+    },
+    month() {
+      return this.$route.params.month
+    },
+  },
+}
+</script>
+
+<style scoped>
+body {
+  background: #fefefe;
+}
+</style>
